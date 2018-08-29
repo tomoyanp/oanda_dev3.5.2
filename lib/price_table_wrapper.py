eb2bf1e21bd58193f33d3f0b00929b8628a3015a
tomoyanpy <tomoyanpy@gmail.com>
2018-08-29 19:20:46 +0900

    modi

--- lib/price_table_wrapper.py
+++ lib/price_table_wrapper.py
@@ -6,22 +6,22 @@ class PriceTableWrapper:
 
 
     def __init__(self):
-    	self.ask_price_list = []
-    	self.bid_price_list = []
-    	self.time_list = []
+        self.ask_price_list = []
+        self.bid_price_list = []
+        self.time_list = []
 
 
     def setResponse(self, response):
-    	for line in reponse:
-    		self.time_list.appned(line[0])
-    		self.ask_price_list.appned(line[1])
-    		self.bid_price_list.appned(line[2])
+        for line in reponse:
+            self.time_list.appned(line[0])
+            self.ask_price_list.appned(line[1])
+            self.bid_price_list.appned(line[2])
 
-	def getTimeList(self):
-		return self.time_list
+    def getTimeList(self):
+        return self.time_list
 
-	def getAskPriceList(self):
-		return self.ask_price_list
+    def getAskPriceList(self):
+        return self.ask_price_list
 
-	def getBidPriceList(self):
-		return self.bid_price_list
+    def getBidPriceList(self):
+        return self.bid_price_list

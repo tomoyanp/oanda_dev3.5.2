965b630804a24ce298fcfc3ff0c1df2285c1937e
tomoyanp <tomoyan@gmail.com>
2018-09-18 11:04:47 +0900

    modi

--- tool/lstm_learning.py
+++ tool/lstm_learning.py
@@ -40,8 +40,8 @@ instrument = "GBP_JPY"
 mysql_connector = MysqlConnector()
 
 def main():
-    learning_model1h = train_save_model(window_size=24, output_train_index=8, table_type="1h", figure_filename="figure_1h.png", model_filename="lstm_1h.json", weights_filename="lstm_1h.hdf5", start_time="2017-03-01 00:00:00", end_time="2018-04-01 00:00:00", term="all")
-    learning_model5m = train_save_model(window_size=8*12, output_train_index=12, table_type="5m", figure_filename="figure_5m.png", model_filename="lstm_5m.json", weights_filename="lstm_5m.hdf5", start_time="2017-03-01 00:00:00", end_time="2018-04-01 00:00:00", term="all")
+#    learning_model1h = train_save_model(window_size=24, output_train_index=8, table_type="1h", figure_filename="figure_1h.png", model_filename="lstm_1h.json", weights_filename="lstm_1h.hdf5", start_time="2017-03-01 00:00:00", end_time="2018-04-01 00:00:00", term="all")
+#    learning_model5m = train_save_model(window_size=8*12, output_train_index=12, table_type="5m", figure_filename="figure_5m.png", model_filename="lstm_5m.json", weights_filename="lstm_5m.hdf5", start_time="2017-03-01 00:00:00", end_time="2018-04-01 00:00:00", term="all")
     learning_model1d = train_save_model(window_size=10, output_train_index=1, table_type="day", figure_filename="figure_1d.png", model_filename="lstm_1d.json", weights_filename="lstm_1d.hdf5", start_time="2017-03-01 00:00:00", end_time="2018-04-01 00:00:00", term="all")
 
 

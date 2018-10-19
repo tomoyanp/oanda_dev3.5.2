de17b967a307681fb8e33274b43fcd3b827860f3
tomoyanpy <tomoyanpy@gmail.com>
2018-10-19 10:31:55 +0900

    modi

--- trade_algorithm/lstm_algo.py
+++ trade_algorithm/lstm_algo.py
@@ -435,9 +435,9 @@ class LstmAlgo(SuperAlgo):
                 self.first_trade_time = base_time
 
         if self.first_trade_flag != "" and minutes % 5 == 0 and 5 < seconds < 15:
-            self.usdjpy_sma = get_sma(instrument="USD_JPY", base_time=base_time, table_type="5m", length=40, con=self.mysql_connector):
-            self.eurusd_sma = get_sma(instrument="EUR_USD", base_time=base_time, table_type="5m", length=40, con=self.mysql_connector):
-            self.eurjpy_sma = get_sma(instrument="EUR_JPY", base_time=base_time, table_type="5m", length=40, con=self.mysql_connector):
+            self.usdjpy_sma = get_sma(instrument="USD_JPY", base_time=base_time, table_type="5m", length=40, con=self.mysql_connector)
+            self.eurusd_sma = get_sma(instrument="EUR_USD", base_time=base_time, table_type="5m", length=40, con=self.mysql_connector)
+            self.eurjpy_sma = get_sma(instrument="EUR_JPY", base_time=base_time, table_type="5m", length=40, con=self.mysql_connector)
         
             if self.first_trade_flag == "buy":
                 if self.usdjpy_current_price > self.usdjpy_sma and self.eurusd_current_price > self.eurusd_sma and self.eurjpy_current_price > self.eurjpy_sma:
@@ -536,9 +536,6 @@ class LstmAlgo(SuperAlgo):
         self.result_logger.info("# usdjpy_sma=%s" % self.usdjpy_sma) 
         self.result_logger.info("# eurusd_sma=%s" % self.eurusd_sma) 
         self.result_logger.info("# eurjpy_sma=%s" % self.eurjpy_sma) 
-        self.result_logger.info("# current_price=%s" % self.current_price)
-        self.result_logger.info("# upper_sigma1h=%s" % self.upper_sigma1h)
-        self.result_logger.info("# lower_sigma1h=%s" % self.lower_sigma1h)
 
     def settlementLogWrite(self, profit, base_time, stl_price, stl_method):
         self.result_logger.info("# %s at %s" % (stl_method, base_time))

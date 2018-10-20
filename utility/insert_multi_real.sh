cc7fc03bf9ff3f4032168564ef2a0bdfdbe28c5f
tomoyanp <tomoyanpy@gmail.com>
2018-10-20 23:56:05 +0900

    omod

--- utility/insert_multi_real.sh
+++ utility/insert_multi_real.sh
@@ -1,3 +1,4 @@
+source /env/python35/bin/activate
 nohup python insert_multi_real_table.py AUD_JPY > aud_jpy.log &
 nohup python insert_multi_real_table.py EUR_JPY > eur_jpy.log &
 nohup python insert_multi_real_table.py GBP_JPY > gbp_jpy.log &

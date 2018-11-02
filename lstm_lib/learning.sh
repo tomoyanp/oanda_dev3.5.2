d5028ff30759bd3b7d71e5e9fc2826806ef75c8e
tomoyanpy <tomoyanpy@gmail.com>
2018-11-02 20:13:11 +0900

    modi

--- lstm_lib/learning.sh
+++ lstm_lib/learning.sh
@@ -1,18 +1,18 @@
-nohup python multi_model.py USD_JPY 1h > usd_jpy_1h.log &
-nohup python multi_model.py GBP_JPY 1h > gbp_jpy_1h.log &
-nohup python multi_model.py EUR_JPY 1h > eur_jpy_1h.log &
-nohup python multi_model.py EUR_USD 1h > eur_usd_1h.log &
-nohup python multi_model.py GBP_USD 1h > gbp_usd_1h.log &
+#nohup python multi_model.py USD_JPY 1h > usd_jpy_1h.log &
+#nohup python multi_model.py GBP_JPY 1h > gbp_jpy_1h.log &
+#nohup python multi_model.py EUR_JPY 1h > eur_jpy_1h.log &
+#nohup python multi_model.py EUR_USD 1h > eur_usd_1h.log &
+#nohup python multi_model.py GBP_USD 1h > gbp_usd_1h.log &
+#
+#nohup python multi_model.py USD_JPY 3h > usd_jpy_3h.log &
+#nohup python multi_model.py GBP_JPY 3h > gbp_jpy_3h.log &
+#nohup python multi_model.py EUR_JPY 3h > eur_jpy_3h.log &
+#nohup python multi_model.py EUR_USD 3h > eur_usd_3h.log &
+#nohup python multi_model.py GBP_USD 3h > gbp_usd_3h.log &
 
-nohup python multi_model.py USD_JPY 3h > usd_jpy_3h.log &
-nohup python multi_model.py GBP_JPY 3h > gbp_jpy_3h.log &
-nohup python multi_model.py EUR_JPY 3h > eur_jpy_3h.log &
-nohup python multi_model.py EUR_USD 3h > eur_usd_3h.log &
-nohup python multi_model.py GBP_USD 3h > gbp_usd_3h.log &
-
-nohup python multi_model.py USD_JPY 8h > usd_jpy_8h.log &
-nohup python multi_model.py GBP_JPY 8h > gbp_jpy_8h.log &
-nohup python multi_model.py EUR_JPY 8h > eur_jpy_8h.log &
-nohup python multi_model.py EUR_USD 8h > eur_usd_8h.log &
-nohup python multi_model.py GBP_USD 8h > gbp_usd_8h.log &
+nohup python multi_model.py USD_JPY day > usd_jpy_day.log &
+nohup python multi_model.py GBP_JPY day > gbp_jpy_day.log &
+nohup python multi_model.py EUR_JPY day > eur_jpy_day.log &
+nohup python multi_model.py EUR_USD day > eur_usd_day.log &
+nohup python multi_model.py GBP_USD day > gbp_usd_day.log &
 

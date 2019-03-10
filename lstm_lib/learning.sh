e2a1aabd025a48e7fc5d74c4d37899802e0c639e
tomoyanp <tomoyanpy@gmail.com>
2019-03-10 09:11:09 +0900

    modi

--- lstm_lib/learning.sh
+++ lstm_lib/learning.sh
@@ -1,1 +1,3 @@
 nohup python multi_model.py 1m 60 60 2019-01-10 2019-03-08 > 1m_learning_log & 
+nohup python multi_model.py 5m 60 12 2018-08-10 2019-03-08 > 5m_learning_log & 
+nohup python multi_model.py 1h 60 1 2018-01-10 2019-03-08 > 1h_learning_log & 

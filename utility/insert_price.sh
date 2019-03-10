9d0976eaff9fb51232d3d7f3c6e8f7e75b1f479c
tomoyanp <tomoyanpy@gmail.com>
2019-03-10 19:35:57 +0900

    modi

--- utility/insert_price.sh
+++ utility/insert_price.sh
@@ -1,4 +1,10 @@
+TODAY=`date +%Y-%m-%d`
+LOGTIME=`date +%Y%m%d%H%M%S`
 source /env/python35/bin/activate
-nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py "GBP_JPY" > /var/log/product/insert_price_gbp_jpy.log &
-sleep 10
-nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_multi_table.py "GBP_JPY" "production" > /var/log/product/insert_multi_gbp_jpy.log & 
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py AUD_JPY $TODAY production> /var/log/product/aud_jpy_$LOGTIME.log &
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py EUR_JPY $TODAY production> /var/log/product/eur_jpy_$LOGTIME.log &
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py GBP_JPY $TODAY production> /var/log/product/gbp_jpy_$LOGTIME.log &
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py EUR_USD $TODAY production> /var/log/product/usd_jpy_$LOGTIME.log &
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py AUD_USD $TODAY production> /var/log/product/aud_usd_$LOGTIME.log &
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py GBP_USD $TODAY production> /var/log/product/gbp_usd_$LOGTIME.log &
+nohup python /home/tomoyan/staging/oanda_dev3.5.2/utility/insert_price.py USD_JPY $TODAY production> /var/log/product/usd_jpy_$LOGTIME.log &

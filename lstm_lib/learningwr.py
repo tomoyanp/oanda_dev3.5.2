de079f961e6b2b670221eee5e108c7102934c2cf
tomoyanpy <tomoyanpy@gmail.com>
2019-04-08 17:01:10 +0900

    mod

--- lstm_lib/learningwr.py
+++ lstm_lib/learningwr.py
@@ -29,7 +29,7 @@ import pandas as pd
 
 
 def __get_price(con, targettime, tabletype, instrument):
-    sql = "select close_ask, close_bid, high_ask, high_bid, low_ask, low_bid from %s_%s_TABLE where insert_time < \'%s\' order by insert_time desc limit 1" % (instrument, tabletype, targettime)
+    sql = "select close_ask, close_bid, high_ask, high_bid, low_ask, low_bid, insert_time from %s_%s_TABLE where insert_time < \'%s\' order by insert_time desc limit 1" % (instrument, tabletype, targettime)
     response = con.select_sql(sql)
     close_ask = response[0][0]
     close_bid = response[0][1]
@@ -37,17 +37,18 @@ def __get_price(con, targettime, tabletype, instrument):
     high_bid = response[0][3]
     low_ask = response[0][4]
     low_bid = response[0][5]
+    insert_time = respones[0][6]
 
-    return close_ask, close_bid, high_ask, high_bid, low_ask, low_bid
+    return close_ask, close_bid, high_ask, high_bid, low_ask, low_bid, insert_time
 
 def __get_dataset(con, instrument, targettime, tabletype, y_index):
-    close_ask, close_bid, high_ask, high_bid, low_ask, low_bid = __get_price(con, targettime, tabletype, instrument)
+    close_ask, close_bid, high_ask, high_bid, low_ask, low_bid, insert_time = __get_price(con, targettime, tabletype, instrument)
     closeprice = (close_ask + close_bid)/2
     high_price = (high_ask + high_bid) / 2
     low_price = (low_ask + low_bid) / 2
     
     bef_targettime = change_to_targettime(targettime, tabletype)
-    bef_close_ask, bef_close_bid, bef_high_ask, bef_high_bid, bef_low_ask, bef_low_bid = __get_price(con, bef_targettime, tabletype, instrument)
+    bef_close_ask, bef_close_bid, bef_high_ask, bef_high_bid, bef_low_ask, bef_low_bid, bef_insert_time = __get_price(con, bef_targettime, tabletype, instrument)
     bef_closeprice = (bef_close_ask + bef_close_bid) / 2
     
     bollinger_dataset = get_bollinger(con, instrument, targettime, tabletype, window_size=21, sigma_valiable=2)
@@ -64,17 +65,28 @@ def __get_dataset(con, instrument, targettime, tabletype, y_index):
          "lowersigma": [],
          "sma25": [],
          "sma75": [],
-         "sma100": []
+         "sma100": [],
+         "bef_insert_time": [],
+         "insert_time": []
         }
-    x["bef_closeprice"] = (bef_closeprice - closeprice)
-    x["high_price"] = (high_price - closeprice)
-    x["low_price"] = (low_price - closeprice)
-    x["uppersigma"] = (uppersigma - closeprice)
-    x["lowersigma"] = (lowersigma - closeprice)
-    x["sma25"] = (sma25 - closeprice)
-    x["sma75"] = (sma75 - closeprice)
-    x["sma100"] = (sma100 - closeprice)
+    x["bef_closeprice"] = float(bef_closeprice)
+    x["high_price"] = float(high_price)
+    x["low_price"] = float(low_price)
+    x["uppersigma"] = float(uppersigma)
+    x["lowersigma"] = float(lowersigma)
+    x["sma25"] = float(sma25)
+    x["sma75"] = float(sma75)
+    x["sma100"] = float(sma100)
+    x["insert_time"] = insert_time
+    x["bef_insert_time"] = bef_insert_time
     
+    x = pd.DataFrame(x)
+    print(x)
+
+    del x["insert_time"]
+    del x["bef_insert_time"]
+    x = x - closeprice
+
     y_targettime = change_to_nexttime(targettime, tabletype, index=y_index)
     y_close_ask, y_close_bid, y_high_ask, y_high_bid, y_low_ask, y_low_bid = __get_price(con, y_targettime, tabletype, instrument)
 #    if close_ask < y_close_bid:
@@ -121,12 +133,18 @@ def get_datasets(con, instrument, starttime, endtime, tabletype, y_index, modeln
 
     return x_list, y_list
 
+def normalize_data(data):
+    means = data.mean()
+    std = data.std()
+    data = (data - means)/(std)
+    
+    return data
 
 if __name__ == "__main__":
     # set parameters
     instrument = "USD_JPY"
-    starttime = "2019-01-01 06:50:00"
-    endtime = "2019-01-01 07:00:00"
+    starttime = "2019-04-08 17:30:00"
+    endtime = "2019-04-08 18:00:00"
     starttime = change_to_ptime(starttime)
     endtime = change_to_ptime(endtime)
     tabletype = "1m"
@@ -134,57 +152,47 @@ if __name__ == "__main__":
 
     con = MysqlConnector()
 
-#    # get train data set
-#    modelname = "svm"
-#    x, y = get_datasets(con, instrument, starttime, endtime, tabletype,  y_index, modelname)
-#    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=None)
-#
-#    tmp = pd.DataFrame(x_train)
-#    print(tmp.shape)
-#    # Normalize Dataset
-#    sc = StandardScaler()
-#    sc.fit(x_train)
-#    x_train_std = sc.transform(x_train)
-#    x_test_std = sc.transform(x_test)
-#
-#    print(len(x_train_std[0]))
-#    print(len(x_train_std))
-#    print(len(y_train))
-#
-#    
-#    ### SVM
-#    # create model
-#    model = SVC(kernel="linear", random_state=None)
-#    # learning
-#    model.fit(x_train_std, y_train)
-#    pred_train = model.predict(x_train_std)
-#    accuracy_train = accuracy_score(y_train, pred_train)
-#    print("svm results = %s" % accuracy_train)
-#    filename = 'svm.sav'
-#    pickle.dump(model, open(filename, 'wb'))
-#
-#    ### Logistic Regression
-#    # create model
-#    model = LogisticRegression(random_state=None)
-#    # learning
-#    model.fit(x_train_std, y_train)
-#    pred_train = model.predict(x_train_std)
-#    accuracy_train = accuracy_score(y_train, pred_train)
-#    print("logistic regression results = %s" % accuracy_train)
-#    filename = 'logistic.sav'
-#    pickle.dump(model, open(filename, 'wb'))
-#
-#    ### Random Forest
-#    # create model
-#    model = RandomForestClassifier()
-#    # learning
-#    model.fit(x_train_std, y_train)
-#    pred_train = model.predict(x_train_std)
-#    accuracy_train = accuracy_score(y_train, pred_train)
-#    print("random forest results = %s" % accuracy_train)
-#    filename = 'rndfrst.sav'
-#    pickle.dump(model, open(filename, 'wb'))
+    # get train data set
+    modelname = "svm"
+    x, y = get_datasets(con, instrument, starttime, endtime, tabletype,  y_index, modelname)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=None)
 
+    # Normalize Dataset
+    x_train_std = normalize_data(np.array(x_train))
+    x_test_std = normalize_data(np.array(x_test))
+
+    ### SVM
+    # create model
+    model = SVC(kernel="linear", random_state=None)
+    # learning
+    model.fit(x_train_std, y_train)
+    pred_train = model.predict(x_train_std)
+    accuracy_train = accuracy_score(y_train, pred_train)
+    print("svm results = %s" % accuracy_train)
+    filename = 'svm.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+    ### Logistic Regression
+    # create model
+    model = LogisticRegression(random_state=None)
+    # learning
+    model.fit(x_train_std, y_train)
+    pred_train = model.predict(x_train_std)
+    accuracy_train = accuracy_score(y_train, pred_train)
+    print("logistic regression results = %s" % accuracy_train)
+    filename = 'logistic.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+    ### Random Forest
+    # create model
+    model = RandomForestClassifier()
+    # learning
+    model.fit(x_train_std, y_train)
+    pred_train = model.predict(x_train_std)
+    accuracy_train = accuracy_score(y_train, pred_train)
+    print("random forest results = %s" % accuracy_train)
+    filename = 'rndfrst.sav'
+    pickle.dump(model, open(filename, 'wb'))
 
     modelname = "lstm"
     window_size = 30
@@ -193,17 +201,10 @@ if __name__ == "__main__":
 
     x, y = get_datasets(con, instrument, starttime, endtime, tabletype,  y_index, modelname, window_size)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=None)
+
     # Normalize Dataset
-    sc = MinMaxScaler(feature_range=(0, 1))
-
-    df_x_train = pd.DataFrame(x_train)
-    df_x_test = pd.DataFrame(x_test)
-
-    print(df_x_train.shape)
-    print(df_x_test.shape)
-
-    x_train_std = sc.fit_transform(df_x_train)
-    x_test_std = sc.fit_transform(df_x_test)
+    x_train_std = normalize_data(np.array(x_train))
+    x_test_std = normalize_data(np.array(x_test))
 
     model = Sequential()
     # add dataset 3dimention size

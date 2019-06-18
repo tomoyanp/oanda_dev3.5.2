c1cb52302bc24a435a241eef2132897ea7232304
tomoyanpy <tomoyanpy@gmail.com>
2019-06-19 01:04:04 +0900

    modi

--- test/test.py
+++ test/test.py
@@ -20,47 +20,79 @@ con = MysqlConnector()
 
 instrument_list = ["EUR_GBP", "EUR_USD", "EUR_JPY", "GBP_USD", "GBP_JPY", "USD_JPY"]
 #instrument_list = ["EUR_USD", "EUR_JPY", "GBP_USD", "GBP_JPY", "USD_JPY"]
-insert_time = '2019-06-13 14:00:00'
+insert_time = '2019-06-18 16:59:00'
 insert_time = datetime.strptime(insert_time, "%Y-%m-%d %H:%M:%S")
 now = datetime.now()
 
-while insert_time < now:
+def decide_trade(length, insert_time, description):
     result_list = []
+    insert_time = insert_time - timedelta(seconds=5)
+
     for instrument in instrument_list:
-        sql = "select open_ask, open_bid, close_ask, close_bid, insert_time from %s_1h_TABLE where insert_time < '%s' order by insert_time desc limit 1" % (instrument, insert_time)
+        sql = "select open_ask, open_bid, close_ask, close_bid, insert_time from %s_5s_TABLE where insert_time <= '%s' order by insert_time desc limit %s" % (instrument, insert_time, length)
         response = con.select_sql(sql)
     
     
-        open_price = (response[0][0]+response[0][1])/2
+        open_price = (response[-1][2]+response[-1][3])/2
         close_price = (response[0][2]+response[0][3])/2
-        result = close_price/open_price
-        res_insert_time = response[0][4]
+        result = (close_price/open_price - 1.0) * 10000
+        start_time = response[-1][4]
+        end_time = response[0][4]
         result_list.append(result)
     
-        print("%s : %s = %s" % (res_insert_time, instrument, result))
-        print("========================================")
+        #print("%s - %s : %s = %s" % (start_time, end_time, instrument, result))
+        #print("========================================")
     
+    value = {}
     
-    if result_list[0] > 1.0 and result_list[1] > 1.0 and result_list[2] > 1.0 and result_list[5] > 1.0:
-        print("%s: EUR_JPY buy" % insert_time)
-    if result_list[0] > 1.0 and result_list[1] > 1.0 and result_list[2] > 1.0 and result_list[5] < 1.0:
-        print("%s: EUR_USD buy" % insert_time)
-    if result_list[0] < 1.0 and result_list[3] > 1.0 and result_list[4] > 1.0 and result_list[5] > 1.0:
-        print("%s: GBP_JPY buy" % insert_time)
-    if result_list[0] < 1.0 and result_list[3] > 1.0 and result_list[4] > 1.0 and result_list[5] < 1.0:
-        print("%s: GBP_USD buy" % insert_time)
+    if result_list[0] > 0.0 and result_list[1] > 0.0 and result_list[2] > 0.0 and result_list[5] > 0.0:
+        value["currency"] = "EUR_JPY"
+        value["key"] = "buy"
+    if result_list[0] > 0.0 and result_list[1] > 0.0 and result_list[2] > 0.0 and result_list[5] < 0.0:
+        value["currency"] = "EUR_USD"
+        value["key"] = "buy"
+    if result_list[0] < 0.0 and result_list[3] > 0.0 and result_list[4] > 0.0 and result_list[5] > 0.0:
+        value["currency"] = "GBP_JPY"
+        value["key"] = "buy"
+    if result_list[0] < 0.0 and result_list[3] > 0.0 and result_list[4] > 0.0 and result_list[5] < 0.0:
+        value["currency"] = "GBP_USD"
+        value["key"] = "buy"
 
 
-    if result_list[0] < 1.0 and result_list[1] < 1.0 and result_list[2] < 1.0 and result_list[5] < 1.0:
-        print("%s: EUR_JPY sell" % insert_time)
-    if result_list[0] < 1.0 and result_list[1] < 1.0 and result_list[2] < 1.0 and result_list[5] > 1.0:
-        print("%s: EUR_USD sell" % insert_time)
-    if result_list[0] > 1.0 and result_list[3] < 1.0 and result_list[4] < 1.0 and result_list[5] < 1.0:
-        print("%s: GBP_JPY sell" % insert_time)
-    if result_list[0] > 1.0 and result_list[3] < 1.0 and result_list[4] < 1.0 and result_list[5] > 1.0:
-        print("%s: GBP_USD sell" % insert_time)
+    if result_list[0] < 0.0 and result_list[1] < 0.0 and result_list[2] < 0.0 and result_list[5] < 0.0:
+        value["currency"] = "EUR_JPY"
+        value["key"] = "sell"
+    if result_list[0] < 0.0 and result_list[1] < 0.0 and result_list[2] < 0.0 and result_list[5] > 0.0:
+        value["currency"] = "EUS_USD"
+        value["key"] = "sell"
+    if result_list[0] > 0.0 and result_list[3] < 0.0 and result_list[4] < 0.0 and result_list[5] < 0.0:
+        value["currency"] = "GBP_JPY"
+        value["key"] = "sell"
+    if result_list[0] > 0.0 and result_list[3] < 0.0 and result_list[4] < 0.0 and result_list[5] > 0.0:
+        value["currency"] = "GBP_USD"
+        value["key"] = "sell"
+
+    value["description"] = description
+    return value
+
+while insert_time < now:
+    length = 2
+    value5s = decide_trade(length, insert_time, "5 seconds")
+
+    length = 12
+    value1m = decide_trade(length, insert_time, "1 minutes")
+
+    length = 12 * 5
+    value5m = decide_trade(length, insert_time, "5 minutes")
+
+    length = 12 * 60
+    value1h = decide_trade(length, insert_time, "1 hour")
+
+    if len(value5s) > 1 and len(value1m) > 1 and len(value5m) > 1 and len(value1h) > 1:
+        if value5s["key"] == value1m["key"] == value5m["key"] == value1h["key"] and value5s["currency"] == value1m["currency"] == value5m["currency"] == value1h["currency"]:
+            print("%s: %s %s" % (insert_time, value5s["currency"], value5s["key"]))
+
+    insert_time = insert_time + timedelta(seconds=5)
 
 
 
-
-    insert_time = insert_time + timedelta(hours=1)

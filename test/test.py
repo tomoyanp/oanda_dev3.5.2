2cf33fa287665a4a0e7502b7626a438f1f4dc133
tomoyanpy <tomoyanpy@gmail.com>
2019-06-19 09:00:57 +0900

    modi

--- test/test.py
+++ test/test.py
@@ -45,6 +45,7 @@ def decide_trade(length, insert_time, description):
     
     value = {}
     
+    # instrument_list = ["EUR_GBP", "EUR_USD", "EUR_JPY", "GBP_USD", "GBP_JPY", "USD_JPY"]
     if result_list[0] > 0.0 and result_list[1] > 0.0 and result_list[2] > 0.0 and result_list[5] > 0.0:
         value["currency"] = "EUR_JPY"
         value["key"] = "buy"
@@ -63,7 +64,7 @@ def decide_trade(length, insert_time, description):
         value["currency"] = "EUR_JPY"
         value["key"] = "sell"
     if result_list[0] < 0.0 and result_list[1] < 0.0 and result_list[2] < 0.0 and result_list[5] > 0.0:
-        value["currency"] = "EUS_USD"
+        value["currency"] = "EUR_USD"
         value["key"] = "sell"
     if result_list[0] > 0.0 and result_list[3] < 0.0 and result_list[4] < 0.0 and result_list[5] < 0.0:
         value["currency"] = "GBP_JPY"
@@ -75,22 +76,85 @@ def decide_trade(length, insert_time, description):
     value["description"] = description
     return value
 
+def trade(insert_time, instrument, trade_side):
+    insert_time = insert_time - timedelta(seconds=5)
+
+    sql = "select close_ask, close_bid from %s_5s_TABLE where insert_time < '%s' order by insert_time desc limit 1" % (instrument, insert_time)
+    response = con.select_sql(sql)
+
+    price = (response[0][0]+response[0][1])/2
+
+    return price, instrument, trade_side
+
+def stl(insert_time, trade_obj, profit_rate, orderstop_rate):
+    insert_time = insert_time - timedelta(seconds=5)
+    sql = "select close_ask, close_bid from %s_5s_TABLE where insert_time < '%s' order by insert_time desc limit 1" % (trade_obj["instrument"], insert_time)
+    response = con.select_sql(sql)
+
+    price = (response[0][0]+response[0][1])/2
+
+    stl_flag = False
+    if trade_obj["side"] == "buy":
+        # ここの計算おかしいからちゃんと調べる
+        pips = (price/trade_obj["price"] - 1.0) * 10000
+    elif trade_obj["side"] == "sell":
+        # ここの計算おかしいからちゃんと調べる
+        pips = (trade_obj["price"]/price - 1.0) * 10000
+    else:
+        raise
+
+
+    if pips > profit_rate:
+        stl_flag = True
+    elif pips < orderstop_rate:
+        stl_flag = True
+
+
+    profit = 0
+    if stl_flag:
+        if trade_obj["side"] == "buy":
+            profit = price - trade_obj["price"]
+        elif trade_obj["side"] == "sell":
+            profit = trade_obj["price"] - price
+
+    return stl_flag, insert_time, price, profit
+
+
+trade_obj = {"flag": False}
+
+profit_rate = 10
+orderstop_rate = -5
 while insert_time < now:
-    length = 2
-    value5s = decide_trade(length, insert_time, "5 seconds")
+    print("%s" % insert_time)
+    if trade_obj["flag"] == False:
+        length = 2
+        value5s = decide_trade(length, insert_time, "5 seconds")
+    
+        length = 12
+        value1m = decide_trade(length, insert_time, "1 minutes")
+    
+        length = 12 * 5
+        value5m = decide_trade(length, insert_time, "5 minutes")
+    
+        length = 12 * 60
+        value1h = decide_trade(length, insert_time, "1 hour")
+    
+        if len(value5s) > 1 and len(value1m) > 1 and len(value5m) > 1 and len(value1h) > 1:
+            if value5s["key"] == value1m["key"] == value5m["key"] == value1h["key"] and value5s["currency"] == value1m["currency"] == value5m["currency"] == value1h["currency"]:
+                print("TRADE %s: %s %s" % (insert_time, value5s["currency"], value5s["key"]))
+                price, instrument, trade_side = trade(insert_time, value5s["currency"], value5s["key"])
+                trade_obj["flag"] = True
+                trade_obj["instrument"] = instrument
+                trade_obj["price"] = price
+                trade_obj["side"] = trade_side
+                trade_obj["trade_time"] = insert_time
 
-    length = 12
-    value1m = decide_trade(length, insert_time, "1 minutes")
-
-    length = 12 * 5
-    value5m = decide_trade(length, insert_time, "5 minutes")
-
-    length = 12 * 60
-    value1h = decide_trade(length, insert_time, "1 hour")
-
-    if len(value5s) > 1 and len(value1m) > 1 and len(value5m) > 1 and len(value1h) > 1:
-        if value5s["key"] == value1m["key"] == value5m["key"] == value1h["key"] and value5s["currency"] == value1m["currency"] == value5m["currency"] == value1h["currency"]:
-            print("%s: %s %s" % (insert_time, value5s["currency"], value5s["key"]))
+    if trade_obj["flag"]:
+        stl_flag, stl_time, price, profit = stl(insert_time, trade_obj, profit_rate, orderstop_rate)
+        if stl_flag:
+            print("SETTLE %s %s - %s: %s - %s, %s %s" % (trade_obj["instrument"], trade_obj["trade_time"], stl_time, trade_obj["price"], price, trade_obj["side"], profit))
+            trade_obj = {"flag": False}
+            
 
     insert_time = insert_time + timedelta(seconds=5)
 

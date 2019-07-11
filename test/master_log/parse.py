68dc5b0276c843039eac191e5ca77edb1f6e6ed6
tomoyanp <tomoyanpy@gmail.com>
2019-07-11 22:19:28 +0900

    modi

---
+++ test/master_log/parse.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+
+import subprocess
+
+
+
+fileList = subprocess.getoutput("ls *.log")
+fileList = fileList.split("\n")
+
+print(fileList)
+
+for fl in fileList:
+    filename = fl.strip()
+    entry_count = subprocess.getoutput("cat %s | grep profit | wc -l" % filename)
+    out = subprocess.getoutput("cat %s | grep profit" % filename)
+    out = out.split("\n")
+    profit = 0
+    for i in out:
+        profit = profit + float(i.split("=")[-1])
+
+    print("%s Profit = %s, Entry count = %s" % (filename, profit, entry_count))
+
+
+
+#print(profit)

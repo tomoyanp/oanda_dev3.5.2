b7d9302b5d248487331e1ec78cacd3345dc1180e
tomoyanpy <tomoyanpy@gmail.com>
2019-10-06 10:36:45 +0900

    modi

--- test/main.py
+++ test/main.py
@@ -559,18 +559,20 @@ if __name__ == "__main__":
                 insidebar_status = inside_bar(price_df.tail(2).reset_index())
                 barbwire_status = barbwire(price_df.tail(1).reset_index())
 
-                if outsidebar_status["status"] and outsidebar_status["direction"] == trade_flags["direction"]:
+                threshold = 0.05
+                ema25 = current_ema_df["ema25"].values[0]
+
+                if outsidebar_status["status"] and outsidebar_status["direction"] == trade_flags["direction"] and abs(current_price - ema25) < threshold:
                     print("============= outside bar ===============")
                     trade_flags["position"] = trade_flags["direction"]
 
-                elif insidebar_status["status"] and insidebar_status["direction"] == trade_flags["direction"]:
+                elif insidebar_status["status"] and insidebar_status["direction"] == trade_flags["direction"] and abs(current_price - ema25) < threshold:
                     print("============= inside bar ===============")
                     trade_flags["position"] = trade_flags["direction"]
 
-                elif barbwire_status["status"] and barbwire_status["direction"] == trade_flags["direction"]:
+                elif barbwire_status["status"] and barbwire_status["direction"] == trade_flags["direction"] and abs(current_price - ema25) < threshold:
                     print("============= barbwire ===============")
                     trade_flags["position"] = trade_flags["direction"]
-
                 else:
                     trade_flags["price_action_count"] += 1
 
